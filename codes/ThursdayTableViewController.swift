3770ce63a4d74643b86150fe9e36ebec73e2eb73
John Detjen <johndetjen@Johns-MacBook-Pro-2.local>
2017-12-08 11:48:18 -0600

    Commit 5

---
+++ codes/ThursdayTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThursdayTableViewController.swift
+//  codes
+//
+//  Created by John Detjen on 12/8/17.
+//  Copyright © 2017 FinleyKnight, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ThursdayTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
